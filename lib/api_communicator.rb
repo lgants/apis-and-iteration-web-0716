e7fabc947311528dbe01301e0a3a908b64447c98
lgants <lgants@gmail.com>
2016-07-26 16:35:25 -0400

    star wars api lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,23 +1,47 @@
 require 'rest-client'
+require 'pry'
+require 'json'
+
 
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+  character_films = Array.new
+  character_hash["results"].each do |object|
+    object.each do |attribute, value|
+      if value == character
+        object["films"].each do |url|
+          film = RestClient.get(url)
+          film_info = JSON.parse(film.body)
+          character_films.push({film_info["title"] => film_info})
+        end
+      end
+    end
+  end
+  character_films
+end
+
   # iterate ove the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
-  # return value of this method should be collection of info about each film. 
+  # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
+  #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+
+def parse_character_movies(character_films)
+  # some iteration magic and puts out the movies in a nice list
+  character_films.each do |film|
+    film.each do |title,info|
+      puts title
+    end
+  end
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
-end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to working with the Star Wars API lab!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased. 
+  puts "Please enter a character"
+  get_character_from_user = gets.chomp
 end
-
